21181ca99cb5f19c0b9ccda196436003ec5f322d
Basyan_ <reisdaniel162@gmail.com>
2025-07-19 16:44:36 -0300

    feat: Resolve Exercício sobre Leitura de Multiplos tipos de Dados

--- src/main/java/com/example/exerciciojava/Exercicios.java
+++ src/main/java/com/example/exerciciojava/Exercicios.java
@@ -1,4 +1,26 @@
 package com.example.exerciciojava;
+import java.util.Locale;
 
 public class Exercicios {
+    public static void main(String[] args) {
+
+        String produto1 = "Computador";
+        String produto2 = "Notebook";
+        int idade = 18;
+        int codigo = 3315;
+        char genero = 'M';
+        double preco1 = 2100.00;
+        double preco2 = 1500.00;
+        double measure = 33.12345;
+
+        System.out.println("Produtos: ");
+        System.out.println(produto1 + ", cujo o preço é igual a: $" + preco1);
+        System.out.println(produto2 + ", cujo o preço é igual a: $" + preco2);
+        System.out.println("Record: " + idade + " anos de idade, code " + codigo + " e genero: " + genero);
+        System.out.printf("Measure com 5 decimais: %.5f \n", measure);
+        System.out.printf("Measure com 3 decimais: %.3f \n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("Measure com 3 decimais e com ponto: %.3f \n", measure);
+
+    }
 }
