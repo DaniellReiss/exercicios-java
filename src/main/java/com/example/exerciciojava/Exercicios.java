381972ca548dcdd70e54c4e933b96e20a9166d8f
Basyan_ <reisdaniel162@gmail.com>
2025-07-19 16:47:09 -0300

    feat: Resolve Exercício básico sobre Leitura de dados e a Soma deles

--- src/main/java/com/example/exerciciojava/Exercicios.java
+++ src/main/java/com/example/exerciciojava/Exercicios.java
@@ -1,26 +1,23 @@
 package com.example.exerciciojava;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercicios {
     public static void main(String[] args) {
 
-        String produto1 = "Computador";
-        String produto2 = "Notebook";
-        int idade = 18;
-        int codigo = 3315;
-        char genero = 'M';
-        double preco1 = 2100.00;
-        double preco2 = 1500.00;
-        double measure = 33.12345;
+        Scanner sc = new Scanner(System.in);
+        int num1, num2, result;
 
-        System.out.println("Produtos: ");
-        System.out.println(produto1 + ", cujo o preço é igual a: $" + preco1);
-        System.out.println(produto2 + ", cujo o preço é igual a: $" + preco2);
-        System.out.println("Record: " + idade + " anos de idade, code " + codigo + " e genero: " + genero);
-        System.out.printf("Measure com 5 decimais: %.5f \n", measure);
-        System.out.printf("Measure com 3 decimais: %.3f \n", measure);
-        Locale.setDefault(Locale.US);
-        System.out.printf("Measure com 3 decimais e com ponto: %.3f \n", measure);
+        System.out.println("Digite o primeiro Número:");
+        num1 = sc.nextInt();
+        System.out.println("Digite o segundo Número:");
+        num2 = sc.nextInt();
+
+        result = num1 + num2;
+
+        System.out.println("A soma dos números é: " + result);
+
+        sc.close();
 
     }
 }
